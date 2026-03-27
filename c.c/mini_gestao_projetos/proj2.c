00c92b277e2f14d2d19ae3c1dcc8a64faa0bc46c
notyalC0 <darksd016@gmail.com>
2026-03-27 11:30:45 -0300

    update: git ignore, and finish proj2

--- c.c/mini_gestao_projetos/proj2.c
+++ c.c/mini_gestao_projetos/proj2.c
@@ -13,21 +13,96 @@ struct Veiculo
 #define N 10
 
 struct Veiculo vagas[N];
-int faturamentoTotal = 0;
+float faturamentoTotal = 0;
 
-void entrada() {
+void registroEntrada()
+{
   int vaga;
   int indice;
-  do {
-    printf("Escolha o numero da vaga: ");
-    scanf("%d", &vaga);
-    indice = vaga - 1;
 
-    if(vagas[indice].status == 1) {
-      printf("Esta vaga esta ocupada");
+  printf("Escolha o numero da vaga: ");
+  scanf("%d", &vaga);
+  indice = vaga - 1;
+
+  if (vaga < 1 || vaga > N)
+  {
+    printf("Vaga inexistente!\n");
+    return;
+  }
+
+  if (vagas[indice].status == 1)
+  {
+    printf("Esta vaga esta ocupada");
+  }
+  else
+  {
+    printf("Digite a placa do veiculo: ");
+    scanf(" %[^\n]s", vagas[indice].placa);
+
+    printf("Digite o Modelo do veiculo: ");
+    scanf(" %[^\n]s", vagas[indice].modelo);
+
+    printf("Digite a hora de entrada: ");
+    scanf("%d", &vagas[indice].horaEntrada);
+
+    vagas[indice].status = 1;
+  }
+}
+
+void registroSaida()
+{
+  int vaga;
+  int indice;
+  int hora;
+  float valor;
+
+  printf("Escolha o numero da vaga: ");
+  scanf("%d", &vaga);
+  indice = vaga - 1;
+
+  if (vaga < 1 || vaga > N)
+  {
+    printf("Vaga inválida!\n");
+    return;
+  }
+
+  if (vagas[indice].status == 1)
+  {
+    printf("Digite a hora de saida: ");
+    scanf("%d", &vagas[indice].horaSaida);
+    hora = vagas[indice].horaSaida - vagas[indice].horaEntrada;
+    if (hora < 0)
+      hora += 24;
+    if (hora == 0)
+      hora = 1;
+    valor = hora * 10.0;
+    faturamentoTotal += valor;
+    vagas[indice].status = 0;
+  }
+  else
+  {
+    printf("opcao indisponivel");
+  }
+}
+
+void listarVagas()
+{
+  for (int i = 0; i < N; i++)
+  {
+    if (vagas[i].status == 0)
+    {
+      printf("Vaga %d: [LIVRE] \n", i + 1);
     }
+    else
+    {
+      printf("Vaga %d: [OCUPADA] - Placa: [%s] \n", i + 1, vagas[i].placa);
+    }
+  }
+}
 
-  } while (vaga < 0 || vaga > 10);
+void relatorioDeFaturamento()
+{
+  printf("O seu faturamento total atual e de: %.2f \n", faturamentoTotal);
 }
 
 int main()
@@ -40,19 +115,19 @@ int main()
   int opcao;
   do
   {
-    printf("Escolha uma opcao: \n 1 - Entrada de Veiculo \n 2 - Saida de Veiculo \n 3 - Listar Vagas \n 4 - Relatorio de Faturamento \n 0 - Sair: \n");
+    printf("Escolha uma opcao: \n 1 - Entrada de Veiculo \n 2 - Saida de Veiculo \n 3 - Listar Vagas \n 4 - Relatorio de Faturamento \n 0 - Sair \n Escolha: ");
     scanf("%d", &opcao);
 
     switch (opcao)
     {
     case 1:
-      entrada();
+      registroEntrada();
       break;
     case 2:
-      saida();
+      registroSaida();
       break;
     case 3:
-      ListarVagas();
+      listarVagas();
       break;
     case 4:
       relatorioDeFaturamento();
